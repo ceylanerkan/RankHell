2a319808bb59d9c2e5c320ff89796296eba938a5
ceylanerkan <erkanceylan333@outlook.com>
2026-07-07 11:30:12 +0300

    backend created

---
+++ Backend/rankback/src/main/java/com/example/rankback/RankbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.rankback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankbackApplication.class, args);
+	}
+
+}
---
+++ Backend/rankback/src/test/java/com/example/rankback/RankbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rankback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
